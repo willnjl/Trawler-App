68a05c87e56b5aad4d7f570a700165003ce57e0b
Will <will_leighton@outlook.com>
2020-10-19 15:10:19 +0100

    Updates

---
+++ precache-manifest.346e9d1a4101818e7fc6a78b414318d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3da09c329ea51f6e935724fea6a8c9cf",
+    "url": "/Trawler-App/index.html"
+  },
+  {
+    "revision": "a99a7679bfdaf0854684",
+    "url": "/Trawler-App/static/css/2.65dbc30e.chunk.css"
+  },
+  {
+    "revision": "0a719400a3ac3c88fb2f",
+    "url": "/Trawler-App/static/css/main.b3a8b261.chunk.css"
+  },
+  {
+    "revision": "a99a7679bfdaf0854684",
+    "url": "/Trawler-App/static/js/2.b499fdd0.chunk.js"
+  },
+  {
+    "revision": "cdef75879235130052acc2626c717c44",
+    "url": "/Trawler-App/static/js/2.b499fdd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a719400a3ac3c88fb2f",
+    "url": "/Trawler-App/static/js/main.de661e32.chunk.js"
+  },
+  {
+    "revision": "633585f9b4cdf4d63804",
+    "url": "/Trawler-App/static/js/runtime-main.1bb273cc.js"
+  }
+]);
