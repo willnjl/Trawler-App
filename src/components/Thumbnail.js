e168c066654791f1ae9111648bcc832b0820e105
Will <will_leighton@outlook.com>
2020-11-03 13:50:04 +0000

    thumnail

--- src/components/Thumbnail.js
+++ src/components/Thumbnail.js
@@ -1,14 +1,16 @@
 import React from "react";
 
 export default function Thumbnail({ url }) {
-  return url ? (
+  return (
     <img
-      src={url}
+      src={
+        url
+          ? url
+          : "https://github.com/willnjl/Trawler-App/blob/master/src/assets/icons8-fishing_boat.png"
+      }
       className="popup-img"
       style={{ maxWidth: "175px" }}
       alt={"Vessel"}
     />
-  ) : (
-    <td>{"no image"}</td>
   );
 }
