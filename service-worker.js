427d18776bddafdc1798606b8f0561b8e7654fa7
Will <will_leighton@outlook.com>
2020-11-05 14:36:26 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Trawler-App/precache-manifest.b1ce57e9fbaa9fa8298ce0f7a64e4258.js"
+  "/Trawler-App/precache-manifest.624ca43b0d232f9602a43dcd4a2e1b46.js"
 );
 
 self.addEventListener('message', (event) => {
